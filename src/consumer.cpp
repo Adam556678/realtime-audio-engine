6872f8088b591fb85423939c26e8f4d59b85bb26
Adam556678 <mahmudramadan75@yahoo.com>
2026-06-30 11:24:32 +0300

    Initialize audio stream

--- src/consumer.cpp
+++ src/consumer.cpp
@@ -12,8 +12,13 @@
 void consume(RingBuffer* buffer){
     HRESULT hr = CoInitialize(nullptr);
 
+    if (FAILED(hr)){
+        std::cerr <<"CoInitialize failed\n";
+        return;
+    }
+    
     IMMDeviceEnumerator* enumerator = nullptr;
-
+    
     hr = CoCreateInstance(
         __uuidof(MMDeviceEnumerator),
         nullptr,
@@ -21,6 +26,12 @@ void consume(RingBuffer* buffer){
         __uuidof(IMMDeviceEnumerator),
         (void**)&enumerator
     );
+
+    if (FAILED(hr)){
+        std::cerr <<"CoCreateInstance failed\n";
+        CoUninitialize();
+        return;
+    }
 
     IMMDevice* device = nullptr;
 
@@ -30,6 +41,11 @@ void consume(RingBuffer* buffer){
         eConsole,
         &device
     );
+
+    if (FAILED(hr)){
+        std::cerr <<"Enumerator failed\n";
+        return;
+    }
 
     enumerator->Release();
 
@@ -41,13 +57,62 @@ void consume(RingBuffer* buffer){
         (void**)&audioClient
     );
 
-    device->Release();
-
-}
-
-void handleComException(HRESULT hr, std::string call){
     if (FAILED(hr)){
-        std::cerr << "CoCreateInstance failed\n";
+        std::cerr <<"Activate failed\n";
         return;
     }
+
+    device->Release();
+    
+    // hnsBufferDuration = 1 second; // Create a buffer that can hold 1 second of sound.
+
+    // get device's preferred format
+    WAVEFORMATEX* format = nullptr;
+    audioClient->GetMixFormat(&format);
+
+    hr = audioClient->Initialize(
+        AUDCLNT_SHAREMODE_SHARED, // mix all sounds together
+        0, //No flags
+        200000, // the amount of time the buffer should be able to hold.
+        0, // Only important in Exclusive Mode
+        format,
+        nullptr
+    );
+
+    if (FAILED(hr))
+    {
+        std::cerr << "Initialize failed\n";
+        CoTaskMemFree(format);
+        audioClient->Release();
+        CoUninitialize();
+        return;
+    }
+
+    // We don't need the format structure anymore.
+    CoTaskMemFree(format);
+
+    IAudioRenderClient* renderClient = nullptr;
+
+    hr = audioClient->GetService(
+        __uuidof(IAudioRenderClient),
+        (void**)&renderClient
+    );
+
+    if (FAILED(hr))
+    {
+        std::cerr << "GetService failed\n";
+        audioClient->Release();
+        CoUninitialize();
+        return;
+    }
+
+    UINT32 bufferFrames = 0;
+    hr = audioClient->GetBufferSize(&bufferFrames);
+
+    if (FAILED(hr))
+    {
+        std::cerr << "GetBufferSize failed\n";
+    }
+
+    
 }
